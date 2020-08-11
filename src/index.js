530f3ae9ec7499433af378c6f0576448feb9d8fa
Daniel Hines <d4hines@gmail.com>
2020-08-10 21:16:01 -0400

    switch to  parsimmon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
-const nearley = require("nearley");
-const grammar = require("./grammar.js");
+const P = require("parsimmon");
+const Parsimmon = P;
 
-// Create a Parser object from our grammar.
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+const ALMParser = P.createLanguage({
 
-// Parse something!
-parser.feed("foo\n");
+});
+function PyX(indent) {
+  return P.createLanguage({
 
-// parser.results is an array of possible parsings.
-console.log(parser.results); // [[[[ "foo" ],"\n" ]]]
+  });
+}
+
+// Start parsing at zero indentation
+let Pythonish = PyX(0);
+
+///////////////////////////////////////////////////////////////////////
+var Lang = Parsimmon.createLanguage({
+  Value: function(r) {
+    return Parsimmon.alt(r.Number, r.Symbol, r.List);
+  },
+  Number: function() {
+    return Parsimmon.regexp(/[0-9]+/).map(Number);
+  },
+  Symbol: function() {
+    return Parsimmon.regexp(/[a-z]+/);
+  },
+  List: function(r) {
+    return Parsimmon.string("(")
+      .then(r.Value.sepBy(r.__))
+      .skip(Parsimmon.string(")"));
+  },
+  _: () => Parsimmon.optWhitespace,
+  __: () => Parsimmon.whitespace
+});
+
+console.log(Lang.Value.tryParse("(list 1 2 foo (list nice 3 56 989 asdasdas))"));
+
+// const testFile = require("fs").readFileSync("../test/test.alm").toString();
+// console.log(testFile);
