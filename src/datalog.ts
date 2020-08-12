8c30b9e70922b233fa617761e4d5332ed73935f7
Daniel Hines <d4hines@gmail.com>
2020-08-11 20:44:07 -0400

    strict mode and negation

--- src/datalog.ts
+++ src/datalog.ts
@@ -1,31 +1,15 @@
 import * as R from "rambda";
-// This is a datalog-without-negation interpreter in Javascript
-
-//     a rule is:
-//     
-//     ((PRED ARG0 ... ARGN)   ;; <- the conclusion
-//     (PRED0 ARG ...)        ;; <- the premises
-//     ...
-//     (PREDN ARG ...))
-//     
-//     arguments are:
-//     
-//     X           ;; variables, which are uppercase symbols
-//     | x           ;; atoms, which are lowercase symbols
-const car = ([x]: any[]) => x;
-const cdr = ([x, ...xs]: any[]) => xs;
-
-const is_var = (s: string): boolean => typeof s === "string" && s[0].toUpperCase() === s[0];
-const is_atom = (s: string): boolean => !is_var(s);
-
-const union = (xs, xy) => R.uniq(R.concat(xs, xy));
-const unions = <T>(xs: T[][]): T[] => {
-    return R.uniq([].concat(...xs));
-};
 
 // Substitutions, implemented as JS objects where keys and values are both strings
 type Subst = { [k: string]: string };
-const assign = (subst: Subst, key, value) => {
+
+type Tuple = string[];
+
+const is_var = (s: any): s is string => typeof s === "string" && s[0].toUpperCase() === s[0];
+const is_negated = (t: Tuple) => t[0] === "-";
+
+
+const assign = (subst: Subst, key: string, value: string) => {
     if (!(key in subst)) {
         return { ...subst, [key]: value };
     } else if (subst[key] === value) {
@@ -36,98 +20,69 @@ const assign = (subst: Subst, key, value) => {
 }
 
 const substitute = ([pred, ...args]: string[], subst: Subst) => {
-    return [pred, ...args.reduce((prev, curr) => {
-        return [...prev, (curr in subst) ? subst[curr] : curr]
-    }, [])];
+    return [pred, ...args.map(x => (x in subst) ? subst[x] : x)];
 }
 
 // Returns false on unification failure.
-const unify = (subst: Subst, args, grounds) => {
+const unify = (subst: Subst, args: string[] | string, grounds: string[] | string): Subst | false => {
     console.log("unifying:", subst, args, grounds);
     if (R.equals(args, grounds)) {
         return subst;
     } else if (is_var(args)) {
-        return assign(subst, args, grounds);
+        return assign(subst, args, grounds as string);
     } else if (
         Array.isArray(args)
         && args.length
         && Array.isArray(grounds)
         && grounds.length
     ) {
-        let x = unify(subst, car(args), car(grounds));
+        const x = unify(subst, R.head(args) as string, R.head(grounds) as string);
         if (x === false) {
             return false;
         } else {
-            return unify(x, cdr(args), cdr(grounds));
+            return unify(x, R.tail(args), R.tail(grounds));
         }
     } else {
         return false;
     }
 }
 
-const query = (conc, premises: any[], facts, subst = {}) => {
+const query = (conc: string[], premises: string[][], facts: string[][], subst: Subst = {}): string[][] => {
     if (premises.length === 0) {
         return [substitute(conc, subst)];
     } else {
-        // console.log("premises", premises);
         const [[pred, ...args], ...prems] = premises;
-        return unions(
-            facts.filter(fact => car(fact) === pred)
-                .map(fact => {
-                    const new_subst = unify(subst, args, cdr(fact));
-                    if (new_subst) {
-                        console.log("new subst", new_subst);
-                        return query(conc, prems, facts, new_subst);
-                    } else {
-                        return []
-                    }
-                })
-        )
+        return facts.filter(fact => R.head(fact) === pred)
+            .map(fact => {
+                const new_subst = unify(subst, args, R.tail(fact));
+                if (new_subst) {
+                    return query(conc, prems, facts, new_subst);
+                } else {
+                    return []
+                }
+            }).reduce(R.union);
     }
 }
 
-const fire = ([conclusion, ...premises], facts) => {
-    return query(conclusion, premises, facts);
-}
-
-const step = (rules, facts) => {
-    for (const rule of rules) {
-        facts = union(facts, fire(rule, facts));
+const step = (rules: string[][][], facts: string[][]) => {
+    for (const [conclusion, ...premises] of rules) {
+        const [negated_prems, positive_prems] = R.partition(is_negated, premises);
+        const positive_facts = query(conclusion, positive_prems, facts);
+        const negative_facts = negated_prems.length
+            ? query(conclusion, negated_prems.map(prem => R.drop(1, prem)), facts)
+            : [];
+        facts = R.union(facts, R.difference(positive_facts, negative_facts));
     }
     return facts;
 }
 
-const deduceAll = (rules, facts) => {
+export const datalog = (rules: string[][][], facts: string[][]): string[][] => {
     const newFacts = step(rules, facts);
     console.log("facts", facts);
     console.log("new facts", newFacts);
     if (R.equals(facts, newFacts)) {
         return facts;
     } else {
-        return deduceAll(rules, newFacts);
+        return datalog(rules, newFacts);
     }
 }
-
-const ex_facts_1 = [
-    ["person", "bob"],
-    ["person", "john"],
-    ["loves", "bob", "john"]
-];
-
-const ex_rules_1 = [
-    [["loves", "X", "X"], ["person", "X"]]
-]
-
-console.log("conjunctive query example", deduceAll(ex_rules_1, ex_facts_1));
-
-const ex_facts_2 = [
-    ["edge", "a", "b"],
-    ["edge", "b", "c"],
-    ["edge", "c", "d"]
-];
-
-const ex_rules_2 = [
-    [["path", "X", "Y"], ["edge", "X", "Y"]],
-    [["path", "X", "Z"], ["path", "X", "Y"], ["path", "Y", "Z"]]
-]
-console.log("transitive closure example", deduceAll(ex_rules_2, ex_facts_2));
