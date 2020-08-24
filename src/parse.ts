c2afb7c37146317d32b0f90a1ae9987a28df07bc
Daniel Hines <d4hines@gmail.com>
2020-08-23 21:29:16 -0400

    finish first pass on parser

--- src/parse.ts
+++ src/parse.ts
@@ -1,91 +1,181 @@
 import * as P from "parsimmon";
 
-const commaSeparated = (p: P.Parser<any>) => P.sepBy1(p, P.regexp(/,\s*/));
+export const Nodes = {
+    Boolean: "Boolean",
+    Identifier: "Identifier",
+    Variable: "Variable",
+    BasicTerm: "BasicTerm",
+    ArithmeticTerm: "ArithmeticTerm",
+    Term: "Term",
+    FunctionTerm: "FunctionTerm",
+    Statics: "Statics",
+    DefinedFluents: "DefinedFluents",
+    BasicFluents: "BasicFluents",
+    ArithmeticExpression: "ArithmeticExpression",
+    FunctionDecl: "FunctionDecl",
+    Sorts: "Sorts",
+    FunctionAssignment: "FunctionAssignment",
+    CausalLaw: "CausalLaw",
+    StateConstraint: "StateConstraint",
+    ExecutabilityCondition: "ExecutabilityCondition",
+    Fact: "Fact",
+    Eq: "=",
+    Neq: "!=",
+};
 
-const section = (header: string, p: P.Parser<any>) => P.string(header).skip(P.whitespace).skip(p);
+const jump = (a: P.Parser<any>, b: P.Parser<any>) =>
+    P.seq(a, b).map(x => x[1]);
 
-const ALM = P.createLanguage({
+const commaSeparated = (p: P.Parser<any>) => P.sepBy1(p, P.regexp(/\s*,\s*/)).skip(P.optWhitespace);
+
+const section = (header: string, p: P.Parser<any>) => jump(
+    P.string(header).wrap(P.optWhitespace, P.optWhitespace),
+    p.skip(P.optWhitespace),
+);
+
+const sepByWhiteSpace = (...ps: P.Parser<any>[]) =>
+    P.seq(
+        P.optWhitespace,
+        ...ps.map(x => x.skip(P.optWhitespace))
+    )
+        .map(([head, ...tail]) => tail)
+
+export const ALM = P.createLanguage({
     True: () => P.string("true"),
     False: () => P.string("false"),
     Boolean: r => P.alt(r.True, r.False)
         .desc("a boolean")
-        .map(Boolean),
-    Integer: () => P.regexp(/(\-)?[0-9]/).map(x => Number(x)).desc("an integer"),
-    Identifier: () => P.regexp(/[a-z]+[A-Za-z0-9]*/).desc("an identifier"),
-    Variable: () => P.regexp(/[A-Z]+[A-Za-z0-9]*/).desc("a variable"),
-    ArithmeticOp: () => P.regexp(/+|\-|\*|\/|mod/),
+        .map(Boolean)
+        .node(Nodes.Boolean),
+    Integer: () => P.regexp(/(\-)?[0-9]+/).map(x => Number(x)).desc("an integer"),
+    Identifier: () => P.regexp(/[a-z]+[A-Za-z0-9]*/).desc("an identifier").node(Nodes.Identifier),
+    Variable: () => P.regexp(/[A-Z]+[A-Za-z0-9]*/).desc("a variable").node("Variable"),
+    ArithmeticOp: () => P.regexp(/\+|\-|\*|\/|mod/).desc("an arithmetic operator (+, -, *, /, or mod)"),
     ComparisonRel: () => P.regexp(/>|>=|<|<=/),
     Eq: () => P.string("="),
     Neq: () => P.string("!="),
-    ArithmeticRel: r => P.alt(r.Eq, r.Neq, r.ComparisonRel),
+    ArithmeticRel: r => P.alt(r.ComparisonRel, r.Eq, r.Neq),
     BasicArithmeticTerm: r => P.alt(r.Variable, r.Identifier, r.Integer),
-    BasicTerm: r => P.alt(r.BasicArithmeticTerm, r.Boolean),
-    ArithmeticTerm: r => P.seq(
+    BasicTerm: r => P.alt(r.BasicArithmeticTerm, r.Boolean).node(Nodes.BasicTerm),
+    ArithmeticTerm: r => sepByWhiteSpace(
         r.BasicArithmeticTerm,
-        P.whitespace,
         r.ArithmeticOp,
-        P.whitespace,
         r.BasicArithmeticTerm
-    ),
-    Term: r => P.alt(r.BasicTerm, r.ArithmeticTerm),
+    ).node(Nodes.ArithmeticTerm),
+    Term: r => P.alt(r.ArithmeticTerm, r.BasicTerm),
+    Negation: () => P.string("-").desc("negation (-)").fallback(null),
     FunctionTerm: r => P.seq(
+        r.Negation,
         r.Identifier,
         commaSeparated(r.Term)
-        .wrap(P.string("("), P.string(")")
-    )),
-    PositiveFunctionLiteral: r => P.alt(r.FunctionTerm, P.seq(r.FunctionTerm, P.optWhitespace, r.Eq, P.optWhitespace, r.Term)),
-    NegativeFunctionLiteral: r => P.alt(P.seq(P.string("-"), r.FunctionTerm), P.seq(r.FunctionTerm, P.optWhitespace, r.Neq, P.optWhitespace, r.Term)),
-    FunctionLiteral: r => P.alt(r.PositiveFunctionLiteral, r.NegativeFunctionLiteral),
-    Literal: r => P.alt(r.FunctionLiteral, P.seq(r.ArithmeticTerm, P.optWhitespace, r.ArithmeticRel, P.optWhitespace, r.ArithmeticTerm)),
+            .wrap(P.string("("), P.string(")")))
+        .node(Nodes.FunctionTerm)
+        .map(({ value: [negated, fn, args], ...node }) => ({
+            value: { negated: Boolean(negated), fn, args },
+            ...node
+        })),
+    FunctionAssignment: r => sepByWhiteSpace(
+        P.alt(r.FunctionTerm, r.Identifier),
+        P.alt(r.Eq, r.Neq),
+        r.Term
+    )
+        .node(Nodes.FunctionAssignment)
+        .map(({ value: [fnTerm, operator, ret], ...node }) => ({
+            value: { fnTerm, operator, ret },
+            ...node
+        })),
+    FunctionLiteral: r => P.alt(r.FunctionAssignment, r.FunctionTerm, r.Identifier),
+    ArithmeticExpression: r => sepByWhiteSpace(r.Term, r.ArithmeticRel, r.Term)
+        .node(Nodes.ArithmeticExpression),
+    Literal: r => P.alt(
+        r.FunctionLiteral,
+        r.ArithmeticExpression,
+    ),
     VarOrID: r => P.alt(r.Variable, r.Identifier),
-    Body: r => commaSeparated(r.Literal),
-    Occurs: r => r.VarOrID.wrap(P.string("occurs("), P.string(")")),
-    CausalLaw: r => P.seq(
-        r.Occurs,
-        P.optWhitespace, P.string("Causes"),
-        P.optWhitespace, r.PositiveFunctionLiteral,
-        P.whitespace, P.string("if"), P.whitespace,
+    Body: r => commaSeparated(r.Literal).skip(P.string(".")).skip(P.optWhitespace),
+    Occurs: r => r.VarOrID.wrap(
+        P.optWhitespace.skip(P.string("occurs(")),
+        P.optWhitespace.skip(P.string(")"))
+    ).skip(P.optWhitespace),
+    CausalLaw: r => sepByWhiteSpace(
+        r.Occurs.skip(P.string("causes")),
+        r.FunctionLiteral,
+        P.string("if"),
         r.Body,
-        P.string(".")
-    ),
-    SCHead: r => P.alt(P.string("false"), r.PositiveFunctionLiteral),
-    StateConstraint: r => P.seq(r.SCHead, P.whitespace, P.string("if"), P.whitespace, r.Body, P.string(".")),
-    ExecutabilityCondition: r => P.seq(
+    ).node(Nodes.CausalLaw)
+        .map(({ value: [occurs, head, _, body], ...node }) => ({
+            value: { occurs, head, body },
+            ...node
+        })),
+    SCHead: r => P.alt(P.string("false"), r.FunctionLiteral),
+    StateConstraint: r => sepByWhiteSpace(
+        r.SCHead,
+        P.string("if"),
+        r.Body,
+    )
+        .node(Nodes.StateConstraint)
+        .map(({ value: [head, _, body], ...node }) => ({
+            value: { head, body },
+            ...node
+        })),
+    ExecutabilityCondition: r => sepByWhiteSpace(
         P.string("impossible"),
-        P.whitespace, r.Occurs,
-        P.whitespace, P.string("if"), P.whitespace,
-        r.ExtendedBody,
+        r.Occurs,
+        P.string("if"),
+        r.Body,
+    ).node(Nodes.ExecutabilityCondition)
+        .map(({ value: [_, occurs, __, body], ...node }) => ({
+            value: { occurs, body },
+            ...node
+        })),
+    Sorts: r => section("sorts", r.SortDecl.many()).node(Nodes.Sorts),
+    SortDecl: r => sepByWhiteSpace(
+        commaSeparated(r.Identifier).skip(P.string("::")),
+        commaSeparated(r.SortName),
+        r.Attributes.fallback(null)
+    ).map(([first, second, attributes]) => ({ first, second, attributes })),
+    Attributes: r => section(
+        "attributes",
+        r.FunctionDecl.wrap(P.optWhitespace, P.optWhitespace).atLeast(1)
     ),
-    Facts: r => P.seq(r.PositiveFunctionLiteral, P.string(".")),
-    ExtendedBody: r => commaSeparated(P.alt(r.Literal, r.Occurs, P.seq(P.string("-"), r.Occurs))),
-    Module: r => section("module", P.seq(r.Identifier, P.whitespace, r.ModuleBody)), 
-    ModuleBody: r => P.seq(
-        P.optWhitespace,
-        r.SortDeclarations.fallback(""),
-        P.optWhitespace,
-        r.Statics.fallback(""),
-        P.optWhitespace,
-        r.Fluents.fallback(""),
-        P.optWhitespace,
-        r.Axioms.fallback(""),
-        P.optWhitespace,
-        r.Initially.fallback(""),
-        P.optWhitespace,
-    ),
-    SortDeclarations: r => section("sorts", r.sortDecl.many()),
-    SortDecl: r => P.seq(commaSeparated(r.Identifier), P.whitespace, P.string("::"), commaSeparated(r.SortName),
-        P.optWhitespace, r.Attributes.fallback("")),
-    Attributes: r => section("attributes", P.sepBy1(r.FunctionDecl, P.newline)),
-    Arguments: r => P.seq(P.sepBy1(r.Identifier, P.regex(/\s*x\s*/)), P.optWhitespace, P.string("->")),
+    Arguments: r => P.sepBy1(r.Identifier, P.regexp(/\s*x\s*/))
+        .skip(P.optWhitespace)
+        .skip(P.string("->"))
+        .skip(P.optWhitespace),
     SortName: r => P.alt(r.Identifier, P.seq(r.Integer, P.string(".."), r.Integer)),
-    Statics: r => section("statics", r.FunctionDecl.many()).node("statics"),
-    Fluents: r => P.seq(r.BasicFluents.fallback(""), r.DefinedFluents.fallback("")),
-    BasicFluents: r => section("basic", r.FunctionDecl.many()).node("basic"),
-    DefinedFluents: r => section("defined", r.FunctionDecl.many()).node("defined"),
-    FunctionDecl: r => P.seq(r.Identifier, P.optWhitespace, P.string(":"), P.optWhitespace, r.Arguments.fallback(""), P.optWhitespace, r.Identifier),
+    Statics: r => section("statics", r.FunctionDecl.many()).node(Nodes.Statics),
+    Fluents: r => section("fluents", P.seq(r.BasicFluents.fallback(null), r.DefinedFluents.fallback(null)))
+        .map(([basic, defined]) => ({ basic, defined })),
+    BasicFluents: r => section("basic", r.FunctionDecl.many()).node(Nodes.BasicFluents),
+    DefinedFluents: r => section("defined", r.FunctionDecl.many()).node(Nodes.DefinedFluents),
+    FunctionDecl: r => sepByWhiteSpace(
+        r.Identifier.skip(P.seq(P.optWhitespace, P.string(":"), P.optWhitespace)),
+        r.Arguments.fallback(null),
+        r.Identifier
+    ).node(Nodes.FunctionDecl)
+        .map(({ name, value: [ident, args, ret], start, end }) => ({
+            name, start, end,
+            value: { ident, args, ret }
+        })),
     Axioms: r => section("axioms", r.Axiom.many()),
-    Axiom: r => P.alt(r.CausalLaw, r.StateConstraint, r.ExecutabilityCondition),
+    Fact: r => r.FunctionLiteral.skip(P.string(".")).node(Nodes.Fact),
+    Axiom: r => P.alt(
+        r.CausalLaw,
+        r.StateConstraint,
+        r.ExecutabilityCondition,
+        r.Fact
+    ),
     Initially: r => section("initially", r.Fact.many()),
+    ModuleBody: r =>
+        sepByWhiteSpace(
+            r.Sorts.fallback(null),
+            r.Statics.fallback(null),
+            r.Fluents.fallback(null),
+            r.Axioms.fallback(null),
+            r.Initially.fallback(null),
+        ),
+    Module: r => section("module", sepByWhiteSpace(r.Identifier, r.ModuleBody))
+        .map(([sorts, statics, fluents, axioms, initially]) => ({
+            sorts, statics, fluents, axioms, initially
+        })),
 });
-
-console.log(ALM.FunctionTerm.tryParse("asdfa(a, a, c)"));
---
+++ test/parse.test.ts
@@ -0,0 +1,284 @@
+import { ALM, Nodes } from "../src/parse";
+import { expect } from "chai";
+
+describe("Parsing", () => {
+    it("Identifier", () => {
+        expect(ALM.Identifier.parse("1asdf").status).to.be.false;
+        expect(ALM.Identifier.parse("asdf1asd444").status).to.be.true;
+    });
+
+    it("Arithmetic Term", () => {
+        expect((ALM.ArithmeticTerm.parse("X % 1") as any)["expected"]).to.deep.equal(
+            ["an arithmetic operator (+, -, *, /, or mod)"]
+        );
+        expect(ALM.ArithmeticTerm.parse("1 + X").status).to.be.true;
+        expect(ALM.ArithmeticTerm.parse("X + 3").status).to.be.true;
+        expect(ALM.ArithmeticTerm.parse("X + X").status).to.be.true;
+    });
+
+    it("Occurs", () => {
+        const results = ALM.Occurs.tryParse("occurs(X)");
+        expect(results.name).to.equal("Variable");
+        expect(results.value).to.equal("X");
+    });
+
+    it("FunctionTerm", () => {
+        const result1 = ALM.FunctionTerm.tryParse("f(A)");
+        expect(result1.value.negated).to.be.false;
+        expect(result1.value.fn.value).to.equal("f");
+        expect(result1.value.args[0].value.value).to.equal("A");
+
+        const result2 = ALM.FunctionTerm.tryParse("-f(A)");
+        expect(result2.value.fn.value).to.equal("f");
+        expect(result2.value.args[0].value.value).to.equal("A");
+        expect(result2.value.negated).to.be.true;
+    });
+
+    it("FunctionAssignment", () => {
+        const result1 = ALM.FunctionAssignment.tryParse("f(A) = g");
+        expect(result1.value.fnTerm.value.fn.value).to.equal("f");
+        expect(result1.value.operator).to.equal(Nodes.Eq);
+        expect(result1.value.fnTerm.value.args[0].value.value).to.equal("A");
+        expect(result1.value.ret.value.value).to.equal("g");
+
+        const result2 = ALM.FunctionAssignment.tryParse("a(X) != b");
+        expect(result2.value.fnTerm.value.fn.value).to.equal("a");
+        expect(result2.value.operator).to.equal(Nodes.Neq);
+        expect(result2.value.fnTerm.value.args[0].value.value).to.equal("X");
+        expect(result2.value.ret.value.value).to.equal("b");
+    });
+
+    it("FunctionLiteral", () => {
+        const result1 = ALM.FunctionLiteral.tryParse("f(A) = g");
+        expect(result1.name).to.equal(Nodes.FunctionAssignment);
+
+        const result2 = ALM.FunctionLiteral.tryParse("f(A)");
+        expect(result2.name).to.equal(Nodes.FunctionTerm);
+    });
+
+    it("ArithmeticExpression", () => {
+        const result1 = ALM.ArithmeticExpression.tryParse(`
+            X + 1 != 7 + 1
+        `);
+        expect(result1.name).to.equal(Nodes.ArithmeticExpression);
+
+        const result2 = ALM.ArithmeticExpression.tryParse(`
+            X != 7 
+        `);
+        expect(result2.name).to.equal(Nodes.ArithmeticExpression);
+    });
+
+    it("Causal Law", () => {
+        const results = ALM.CausalLaw.tryParse(`
+        occurs(X) causes f(A) = g if 
+            f(X) = Y,
+            -g(Y),
+            Y + 3 = Z,
+            X = Z.
+        `);
+
+        expect(results.value.occurs.name).to.equal(Nodes.Variable);
+        expect(results.value.head.name).to.equal(Nodes.FunctionAssignment);
+        expect(results.value.body.map((x: any) => x.name)).to.deep.equal([
+            Nodes.FunctionAssignment,
+            Nodes.FunctionTerm,
+            Nodes.ArithmeticExpression,
+            Nodes.ArithmeticExpression
+        ]);
+
+    });
+
+    it("SCHead", () => {
+        ALM.SCHead.tryParse("false");
+        ALM.SCHead.tryParse("f(g) = h");
+        ALM.SCHead.tryParse("f(g)");
+        ALM.SCHead.tryParse("f");
+    });
+
+    it("State Constraint", () => {
+        const results = ALM.StateConstraint.tryParse(`
+        f(A) = g if
+            f(X) = Y,
+            -g(Y),
+            Y + 3 = Z,
+            X = Z.
+        `);
+        expect(results.value.head.name).to.equal(Nodes.FunctionAssignment);
+        expect(results.value.body.map((x: any) => x.name)).to.deep.equal([
+            Nodes.FunctionAssignment,
+            Nodes.FunctionTerm,
+            Nodes.ArithmeticExpression,
+            Nodes.ArithmeticExpression
+        ]);
+    });
+
+    it("Executability Condition", () => {
+        const results = ALM.ExecutabilityCondition.tryParse(`
+        impossible occurs(A) if
+            f(X) = Y,
+            -g(Y),
+            Y + 3 = Z,
+            X = Z.
+        `);
+
+        expect(results.value.occurs.name).to.equal(Nodes.Variable);
+        expect(results.value.body.map((x: any) => x.name)).to.deep.equal([
+            Nodes.FunctionAssignment,
+            Nodes.FunctionTerm,
+            Nodes.ArithmeticExpression,
+            Nodes.ArithmeticExpression
+        ]);
+    });
+
+
+    it("Attributes", () => {
+        const result = ALM.Attributes.tryParse(`
+        attributes
+            a : b x c -> d
+            foo : bar
+        `);
+
+        expect(result[0].value.ident.value).to.equal("a");
+        expect(result[0].value.args.map(({ value }: any) => value))
+            .to.deep.equal(["b", "c"]);
+        expect(result[0].value.ret.value).to.equal("d");
+
+        expect(result[1].value.ident.value).to.equal("foo");
+        expect(result[1].value.args).to.equal(null);
+        expect(result[1].value.ret.value).to.equal("bar");
+    });
+
+    it("Body", () => {
+        const results = ALM.Body.tryParse(`
+        f(X) = Y,
+        -g(Y),
+        Y + 3 = Z,
+        X = Z.
+        `);
+        expect(results.map((x: any) => x.name)).to.deep.equal([
+            Nodes.FunctionAssignment,
+            Nodes.FunctionTerm,
+            Nodes.ArithmeticExpression,
+            Nodes.ArithmeticExpression
+        ]);
+    });
+
+    it("Sorts", () => {
+        const results = ALM.Sorts.tryParse(`
+        sorts
+            foo :: bar
+            a, b :: c, d, e
+                attributes
+                    f : booleans
+        `);
+        const a = results.value[0];
+        expect(a.first[0].value).to.equal("foo");
+        expect(a.second[0].value).to.equal("bar");
+        expect(a.attributes).to.be.null;
+
+        const b = results.value[1];
+        expect(b.first.map(({ value }: any) => value)).to.deep.equal(["a", "b"])
+        expect(b.second.map(({ value }: any) => value)).to.deep.equal(["c", "d", "e"])
+        expect(b.attributes[0].value.ident.value).to.equal("f")
+        expect(b.attributes[0].value.args).to.be.null;
+        expect(b.attributes[0].value.ret.value).to.equal("booleans")
+    });
+
+    it("Statics", () => {
+        const results = ALM.Statics.tryParse(`
+        statics
+            f : g
+        `);
+
+        expect(results.value[0].value.ident.value).to.equal("f");
+        expect(results.value[0].value.args).to.be.null;
+        expect(results.value[0].value.ret.value).to.equal("g");
+    });
+
+    it("Fluents", () => {
+        const results = ALM.Fluents.tryParse(`
+        fluents
+            basic
+                f : g
+            defined
+                a : b
+        `);
+
+        const basicFluent = results.basic.value[0].value;
+        expect(basicFluent.ident.value).to.equal("f");
+        expect(basicFluent.args).to.be.null;
+        expect(basicFluent.ret.value).to.equal("g");
+
+        const definedFluent = results.defined.value[0].value;
+        expect(definedFluent.ident.value).to.equal("a");
+        expect(definedFluent.args).to.be.null;
+        expect(definedFluent.ret.value).to.equal("b");
+    });
+
+    it("FunctionDecl", () => {
+        const ex1: any = ALM.FunctionDecl.parse(`
+            f : a x b -> c
+        `);
+        expect(ex1.value.value.ident.value).to.equal("f");
+        expect(ex1.value.value.args.map(({ value }: any) => value))
+            .to.deep.equal(["a", "b"]);
+        expect(ex1.value.value.ret.value).to.equal("c");
+
+        const ex2: any = ALM.FunctionDecl.parse(`
+            f : a
+        `);
+        expect(ex2.value.value.ident.value).to.equal("f");
+        expect(ex2.value.value.args).to.equal(null);
+        expect(ex2.value.value.ret.value).to.equal("a");
+    });
+
+    it("Axioms", () => {
+        const result = ALM.Axioms.tryParse(`
+        axioms
+            f(a) = 10.
+            x if y.
+            occurs(A) causes f(X) if y.
+            impossible occurs(A) if y.
+        `);
+
+        expect(result.map((x: any) => x.name)).to.deep.equal([
+            Nodes.Fact, Nodes.StateConstraint, Nodes.CausalLaw, Nodes.ExecutabilityCondition
+        ]);
+
+    });
+
+    it("Initially", () => {
+        const result = ALM.Initially.tryParse(`
+        initially
+            f = x.
+            g(10) = -11.
+        `);
+        expect(result.map((x: any) => x.name)).to.deep.equal([
+            Nodes.Fact, Nodes.Fact
+        ]);
+    });
+
+    it("Module", () => {
+        const result = ALM.Module.tryParse(`
+        module myModule
+        sorts
+            a :: b
+        statics
+            s : a x b -> booleans
+        fluents
+            basic
+                foo : a
+            defined
+                bar : b
+        axioms
+            foo = 1 if bar = 2.
+        initially
+            f = x.
+            g(10) = -11.
+        `);
+
+        expect(result).to.have.keys([
+            "sorts", "statics", "fluents", "axioms", "initially"
+        ]);
+    });
+});
