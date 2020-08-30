dfa565d6c16a8e978be11099569ae76efdb8b25e
Daniel Hines <d4hines@gmail.com>
2020-08-29 20:49:04 -0400

    wip

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { Provider, createSession, useDispatch, useQuery, FlamingoValue } from "./runtime";
--- src/runtime.tsx
+++ src/runtime.tsx
@@ -1,19 +1,31 @@
-import React, { createContext, FunctionComponent } from "react";
+import React, { createContext, FunctionComponent, useContext } from "react";
 import { create } from "tau-prolog";
 import { parseModule } from "./parse";
 import { printModule } from "./projection_printer";
 
 export type FlamingoSession = ReturnType<typeof create>;
+
+export type FlamingoValue = string | boolean | number;
+
 export const createSession = (logic: string) => {
     const s = create();
     s.consult(printModule(parseModule(logic)));
     return s;
 }
 
+const SessionContext = createContext<FlamingoSession>(undefined as unknown as FlamingoSession);
+
 export const Provider: FunctionComponent<{session: FlamingoSession}> = ({session, children}) => {
-    const ctx = createContext(session);
-    
-    return <ctx.Provider value={session}>
+    return <SessionContext.Provider value={session}>
         {children}
-    </ctx.Provider>
+    </SessionContext.Provider>
 }
+
+export const useQuery = (query: string): Record<string, FlamingoValue | FlamingoValue[]> => {
+    return {}
+};
+
+export const useDispatch = () => {
+    const session = useContext(SessionContext);
+    return (action: string, attributes?: Record<string, FlamingoValue>) => {}
+};
